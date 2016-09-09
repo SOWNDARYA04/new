5a5013e8a08926d934fc9dd29e43653daaddaf92
SOWNDARYA04 <sowndabi@gmail.com>
2016-09-09 17:48:00 +0530

    Create joy.java

---
+++ joy.java
@@ -0,0 +1,20 @@
+package com.java2novice.algos;
+ import java.io.*;
+public class NumberReverse {
+ 
+    public int reverseNumber(int number){
+         
+        int reverse = 0;
+        while(number != 0){
+            reverse = (reverse*10)+(number%10);
+            number = number/10;
+        }
+        return reverse;
+    }
+     
+    public static void main(String a[]){
+        NumberReverse nr = new NumberReverse();
+        System.out.println("Result: "+nr.reverseNumber(17868));
+    }
+}
+- See more at: http://www.java2novice.com/java-interview-programs/reverse-number/#sthash.wKTcvQMn.dpuf
